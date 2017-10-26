2cc50883b70d9a6176286054e3a138a610e276d4
Sophie Welber <sjwelber@gmail.com>
2017-10-26 16:36:10 -0500

    Set up SoundManager class

---
+++ java/src/edu/virginia/engine/util/SoundManager.java
@@ -0,0 +1,18 @@
+package edu.virginia.engine.util;
+
+import javax.sound.sampled.Clip;
+import java.util.HashMap;
+
+public class SoundManager {
+    private String id;
+    private String filename;
+    private HashMap <String, Clip> sounds;
+
+    //constructor
+
+    //getters and setters for id and filename
+
+    public void LoadSoundEffect(String id, String filename) {
+
+    }
+}
