7aaa7deb4997303c36ff446bf168b10a983bdf2a
molly-odonnell <32308648+molly-odonnell@users.noreply.github.com>
2017-11-01 18:38:56 -0500

    new resources/updated sound

--- java/src/edu/virginia/engine/util/SoundManager.java
+++ java/src/edu/virginia/engine/util/SoundManager.java
@@ -2,61 +2,75 @@ package edu.virginia.engine.util;
 
 import javax.sound.sampled.*;
 import java.util.HashMap;
-import java.io.File;
 import java.io.*;
+import java.net.*;
 
 public class SoundManager {
-    private String id;
-    private String filename;
     private HashMap <String, Clip> soundeffects;
     private Clip music;
 
     //constructor
 
-    //getters and setters for id and filename
-    public String getId() {
-        return this.id;
-    }
-
-    public void setId(String i) {
-        this.id = i;
-    }
-
-    public String getFilename() {
-        return this.filename;
-    }
-
-    public void setFilename(String fn) {
-        this.filename = fn;
-    }
-
 
     public void LoadSoundEffect(String id, String filename) {
         try {
-        File file = new File(filename);
-        AudioInputStream audioIn = AudioSystem.getAudioInputStream(file);
-        Clip clip = AudioSystem.getClip();
-        clip.open(audioIn);
-        this.soundeffects.put(id, clip);
+            File file = new File(filename);
+            if(file.exists()){
+                URL url = this.getClass().getClassLoader().getResource(filename);
+                AudioInputStream audioIn = AudioSystem.getAudioInputStream(url);
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioIn);
+                this.soundeffects.put(id, clip);
+                System.out.println("SUCCESS");
+            } else {
+                System.out.println("ERROR: FILE DOES NOT EXIST");
+            }
+
+//        if (file.exists()) {
+//            AudioInputStream audioIn = AudioSystem.getAudioInputStream(file);
+//            Clip clip = AudioSystem.getClip();
+//            clip.open(audioIn);
+//            this.soundeffects.put(id, clip);
+//            System.out.println("SUCCESS");
+//        } else {
+//            System.out.println("ERROR: FILE DOES NOT EXIST");
+//        }
+         /*
+         URL url = this.getClass().getClassLoader().getResource("gameover.wav");
+
+         // Get a sound clip resource.
+         Clip clip = AudioSystem.getClip();
+         // Open audio clip and load samples from the audio input stream.
+         clip.open(audioIn);
+         clip.start();
+          */
         } catch (UnsupportedAudioFileException ex) {
-            ex.printStackTrace();
+            System.out.println("\nUnsupported Audio");
         } catch (IOException ex) {
-            ex.printStackTrace();
+            System.out.println("\nIO");
         } catch (LineUnavailableException ex) {
-            ex.printStackTrace();
+            System.out.println("\nLineUnavailable");
         }
     }
 
     public void PlaySoundEffect(String id) {
-        this.soundeffects.get(id).start();
+        if(this.soundeffects.get(id)!= null) {
+            this.soundeffects.get(id).start();
+        } else {
+            System.out.println("NULL SOUND EFFECT");
+        }
     }
 
     public void LoadMusic(String id, String filename){
         try {
             File soundFile = new File(filename);
-            AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
-            this.music = AudioSystem.getClip();
-            this.music.open(audioIn);
+            if(soundFile.exists()) {
+                AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
+                this.music = AudioSystem.getClip();
+                this.music.open(audioIn);
+            } else {
+                System.out.println("ERROR: FILE DOES NOT EXIST");
+            }
         }catch (UnsupportedAudioFileException ex) {
         ex.printStackTrace();
     } catch (IOException ex) {
