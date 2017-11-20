48d8bceebff71f683c18be47fb0f10ea02355596
Sophie Welber <sjwelber@gmail.com>
2017-11-19 21:40:20 -0600

    Moved setting of all variables into FinalProject constructor

--- FinalProject/src/edu/virginia/FinalProject.java
+++ FinalProject/src/edu/virginia/FinalProject.java
@@ -20,85 +20,109 @@ import edu.virginia.engine.util.Moves;
 public class FinalProject extends Game{
 
     /* Sprites */
-    Sprite allLevels = new Sprite("All Levels", "levels.png");
+    Sprite allLevels;
+    Sprite moves;
+    Sprite hero;
 
-    Sprite moves = new Sprite("Move Board", "moves.png");
-
-    Sprite hero = new Sprite("Hero", "character.png");
-
+    /* Various game states and trackers */
+    int mvsCount;
+    int currLev;
 
     /* Move Buttons */
-    JButton turn = new JButton(new ImageIcon("resources/turn.png"));
-    JButton fwd = new JButton(new ImageIcon("resources/forward.png"));
-
+    JButton turn;
+    JButton fwd;
 
     /* List of Levels */
-    ArrayList<Level> Levels = new ArrayList<Level> ();
-
+    ArrayList<Level> Levels;
 
     /* Variables to keep track of where things are on the screen */
-    //side bar
-    private int sideBarWidth = 470;
-    private int sideBarHeight = 728;
+    private int sideBarWidth;
+    private int sideBarHeight;
 
-    //maze
-    private int mazeWidth = 470;
-    private int mazeHeight = 728;
+    private int mazeWidth;
+    private int mazeHeight;
 
-    //whole game
-    private int gameWidth = 940;
-    private int gameHeight = 728;
+    private int gameWidth;
+    private int gameHeight;
 
-    //character
-    private int charWidth = 96;
-    private int charHeight = 103;
+    private int charWidth;
+    private int charHeight;
 
-    //grid squares
-    private int sqWidth = 117;
-    private int sqHeight = 117;
+    private int sqWidth;
+    private int sqHeight;
 
-    //gray border
-    private int borderWidth = 32;
+    private int borderWidth;
+
+
 
     /* Constructor */
     public FinalProject() {
         super("Final Project", 940, 728);
 
-        /* Permanent Items */
-        moves.setPosition(new Point(470,0));
+        /* Sprites */
+            this.allLevels = new Sprite("All Levels", "levels.png");
+            this.moves = new Sprite("Move Board", "moves.png");
+                this.moves.setPosition(new Point(470,0));
+            this.hero = new Sprite("Hero", "character.png");
 
-        /* Creating Levels */
-        /* Level 1 */
-        //initial grid
-        ArrayList<Tuple<Boolean, Obstacles>> initGrid = createInitGrid();
-        initGrid.set(2, new Tuple<Boolean, Obstacles>(false, Obstacles.WALL));
-        initGrid.set(3, new Tuple<Boolean, Obstacles>(false, Obstacles.WALL));
-        initGrid.set(6, new Tuple<Boolean, Obstacles>(false, Obstacles.WALL));
-        initGrid.set(7, new Tuple<Boolean, Obstacles>(false, Obstacles.WALL));
-        initGrid.set(12, new Tuple<Boolean, Obstacles>(false, Obstacles.WALL));
-        initGrid.set(16, new Tuple<Boolean, Obstacles>(false, Obstacles.WALL));
-        initGrid.set(18, new Tuple<Boolean, Obstacles>(false, Obstacles.WALL));
-        initGrid.set(23, new Tuple<Boolean, Obstacles>(true, Obstacles.GOAL));
+        /* Game States*/
+            this.mvsCount = 0;
+            this.currLev = 1;
 
-        //available moves
-        ArrayList<JButton> mvsAvail = new ArrayList<JButton>();
-        mvsAvail.add(turn);
-        mvsAvail.add(fwd);
+        /* Move Buttons */
+            this.turn = new JButton(new ImageIcon("resources/turn.png"));
+            this.fwd = new JButton(new ImageIcon("resources/forward.png"));
 
-        //position
-        Point position = new Point(0, 0);
+        /* ArrayList of levels */
+            this.Levels = new ArrayList<Level> ();
 
-        //creating level 1
-        Level lev1 = new Level(initGrid, mvsAvail, position, 1);
+        /* Positions on screen */
+            this.sideBarWidth = 470;
+            this.sideBarHeight = 728;
+            this.mazeWidth = 470;
+            this.mazeHeight = 728;
+            this.gameWidth = 940;
+            this.gameHeight = 728;
+            this.charWidth = 96;
+            this.charHeight = 103;
+            this.sqWidth = 117;
+            this.sqHeight = 117;
+            this.borderWidth = 32;
 
-        Levels.add(lev1);
+        /* Levels */
+            /* Level 1 */
+            //initial grid
+            ArrayList<Tuple<Boolean, Obstacles>> initGrid = createInitGrid();
+            initGrid.set(2, new Tuple<Boolean, Obstacles>(false, Obstacles.WALL));
+            initGrid.set(3, new Tuple<Boolean, Obstacles>(false, Obstacles.WALL));
+            initGrid.set(6, new Tuple<Boolean, Obstacles>(false, Obstacles.WALL));
+            initGrid.set(7, new Tuple<Boolean, Obstacles>(false, Obstacles.WALL));
+            initGrid.set(12, new Tuple<Boolean, Obstacles>(false, Obstacles.WALL));
+            initGrid.set(16, new Tuple<Boolean, Obstacles>(false, Obstacles.WALL));
+            initGrid.set(18, new Tuple<Boolean, Obstacles>(false, Obstacles.WALL));
+            initGrid.set(23, new Tuple<Boolean, Obstacles>(true, Obstacles.GOAL));
 
-    /* Level 2 */
+            //available moves
+            ArrayList<JButton> mvsAvail = new ArrayList<JButton>();
+            mvsAvail.add(turn);
+            mvsAvail.add(fwd);
 
-    /* Level 3 */
+            //position
+            Point position = new Point(0, 0);
+
+            //creating level 1
+            Level lev1 = new Level(initGrid, mvsAvail, position, 1);
+
+            this.Levels.add(lev1);
+
+        /* Level 2 */
+
+        /* Level 3 */
+
+
     }
 
-    //Helper to create and zero out initial grid
+    /* Create and zero out initial grid */
     public ArrayList<Tuple <Boolean, Obstacles>> createInitGrid() {
         ArrayList <Tuple <Boolean, Obstacles>> grid = new ArrayList <Tuple <Boolean, Obstacles>>(24);
         int i;
@@ -107,12 +131,22 @@ public class FinalProject extends Game{
         return grid;
     }
 
+    /* Run through list of moves, performing one move per frame */
+    public void runMoves() {
+        
+        //iterate through movesTaken (one update at a time, use mvsCount)
+        //update character's pos based on mvsCount and Levels[currLev].getMovesTaken()
+
+
+    }
+
     @Override
     public void update(ArrayList<Integer> pressedKeys){
         super.update(pressedKeys);
 
 		/* Make sure mario is not null. Sometimes Swing can auto cause an extra frame to go before everything is initialized */
 
+        //if(clicked on run button) {runMoves();}
     }
 
 
@@ -134,9 +168,6 @@ public class FinalProject extends Game{
         if (moves != null) moves.draw(g);
 
     }
-
-
-
 
     public static void main(String[] args) {
         edu.virginia.FinalProject game = new edu.virginia.FinalProject();
